5ffc26be2bb690cae3b6810e4b0a793c911db38d
ajrfyd <ajrfyd@naver.com>
2022-07-12 23:09:28 +0900

    Update: Home Section update

--- src/Container/Home/Home.tsx
+++ src/Container/Home/Home.tsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from 'react';
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import Notification from "../../components/Notification/Notification";
 import { commonContainerStyle, commonParallStyle } from '../../Style/styles';
+import { CgChevronDoubleDown } from 'react-icons/cg';
 
 type HomeProps = {
   // visible: boolean;
@@ -31,10 +32,14 @@ const Home = ({ setVisible }: HomeProps) => {
 
   return (
     <Container>
-      <Parall1 style={{ transform: `translateX(${-position}px)` }}>
-        즐기며 성장하는 개발자 이홍경 입니다. 
+      <ScrollDownContainer style={{ transform: `translateX(${position}px)`}}>
+        <span>Scroll Down</span>
+        <CgChevronDoubleDown/>  
+      </ScrollDownContainer>
+      <Parall1 style={{ transform: `translateX(${-position}px)`, opacity: (position) / 50 }}>
+        개발을 즐기며 성장하는 <span>이홍경</span> 입니다. 
       </Parall1>
-      <Parall2 style={{ transform: `translateX(${position}px)` }}>
+      <Parall2 style={{ transform: `translateX(${position}px)`, opacity: (position - 100) / 50 }}>
         즐거운 코딩으로 행복을 추구합니다.
       </Parall2>
       <Parall3 style={{ opacity: (position - 500) / 50 }}>
@@ -47,6 +52,20 @@ const Home = ({ setVisible }: HomeProps) => {
 
 export default Home;
 
+const bounce = keyframes`
+  0% {
+    transform: translateY(0);
+  }
+
+  50% {
+    transform: translateY(10px);
+  }
+
+  100% {
+    transform: translate(0);
+  }
+`
+
 const Container = styled.div`
   ${commonContainerStyle};
   width: 100%;
@@ -55,11 +74,37 @@ const Container = styled.div`
   /* height: 100%; */
 `
 
+const ScrollDownContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  gap: .5rem;
+  margin-top: 2rem;
+
+  span {
+    font-weight: bold;
+    font-size: 2rem;
+  }
+
+  svg {
+    font-size: 1.5rem;
+    transition: .1s;
+    animation: ${bounce} 1s infinite;
+  }
+`
+
 const Parall1 = styled.h1`
   ${commonParallStyle};
   position: relative;
   top: 10%;
   left: 60%;
+  
+  span {
+    color: #000;
+    letter-spacing: 5px;
+    font-size: 60px;
+    border-bottom: 5px dotted #ddd; 
+  }
 `
 
 const Parall2 = styled.h1`
@@ -67,6 +112,7 @@ const Parall2 = styled.h1`
   position: absolute;
   right: 60%;
   top: 500px;
+  opacity: 1;
 `
 
 const Parall3 = styled.h1`
--- src/Container/Nav/MiniProfile.tsx
+++ src/Container/Nav/MiniProfile.tsx
@@ -1,12 +1,27 @@
 import styled from "styled-components";
+import { useQuery } from "react-query";
+import axios from "axios";
+
+
+type Profile = {
+  url: string;
+};
 
 const MiniProfile = () => {
+  const getProfile = async () => {
+    const { data } = await axios.get('https://api.github.com/users/ajrfyd');
+    return data;
+  };
+
+  const { data, status } = useQuery('getProfile', getProfile);
+
+  if(!data) return null;
 
   return (
     <Container>
       <ProfileContainer>
-        <Profile />
-        <span>My name is HongKyeong Lee</span>
+        <Profile src={data.avatar_url}/>
+        <span>{data.bio}</span>
       </ProfileContainer>
     </Container>
   )
@@ -17,7 +32,7 @@ export default MiniProfile;
 const Container = styled.div`
   border-radius: 5px;
   box-shadow: 0 0 5px rgba(0, 0, 0, .2);
-  margin: 4rem 0;
+  margin: 3rem 0;
   padding: 3rem 2rem;
 `
 
@@ -25,12 +40,17 @@ const ProfileContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  
+  gap: 1rem;
+
+  span {
+    font-weight: bold;
+    font-size: 1.2rem;
+  }
 `
 
 const Profile = styled.img`
   border: 3px solid #ddd;
-  width: 50px;
-  height: 50px;
+  width: 100px;
+  height: 100px;
   border-radius: 50%;
 `
