51ccc5b1c182c7811897c77cb8d176b615b65b4a
ajrfyd <ajrfyd@naver.com>
2022-07-13 13:07:26 +0900

    Test: Notion Page render

--- src/components/Profiile/Profile.tsx
+++ src/components/Profiile/Profile.tsx
@@ -1,11 +1,29 @@
-import React from "react";
+import { useState, useEffect } from 'react';
 import styled from "styled-components";
 import { commonContainerStyle } from "../../Style/styles";
+import "react-notion/src/styles.css";
+import "prismjs/themes/prism-tomorrow.css"; // only needed for code highlighting
+import { NotionRenderer } from "react-notion";
+import axios from "axios";
 
 const Profile = () => {
+  const address = `https://notion-api.splitbee.io/v1/page/${process.env.REACT_APP_NOTION_ID}`;
+  const [data, setData] = useState({});
 
+  const getNotionData = async () => {
+    const { data } = await axios.get(address);
+    setData(data);
+  };
+
+  useEffect(() => {
+    getNotionData();
+  }, [])
+
+  console.log(data);
+  
   return (
     <Container>
+      <NotionRenderer blockMap={data}/>
     </Container>
   )
 }
